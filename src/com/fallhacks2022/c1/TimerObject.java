d5d9cb9f57253fc2aa6ad60c0d1fa28f3ad1e820
Wilson Agyapong <wilsonagyapong16@gmail.com>
2022-10-15 15:25:40 -0700

    Implemented pause button and resume button

--- src/com/fallhacks2022/c1/TimerObject.java
+++ src/com/fallhacks2022/c1/TimerObject.java
@@ -8,28 +8,9 @@ public class TimerObject {
 
     int workMins, breakMins, longBreakMins, breakItervals;
     int currMins, currSeconds;
+    Timer t;
 
-    Timer t = new Timer();
-    TimerTask task = new TimerTask() {
-        public void run() {
-            if(currSeconds == 0)
-            {
-                currMins--;
-                currSeconds = 60;
-            }
 
-            currSeconds--;
-
-            if(currSeconds < 10) {
-                MainApplication.app.UpdateTimer(currMins + " : 0" + currSeconds);
-            }
-            else
-            {
-                MainApplication.app.UpdateTimer(currMins + " : " + currSeconds);
-            }
-        }
-
-    };
 
     public void pause(){
         t.cancel();
@@ -37,14 +18,41 @@ public class TimerObject {
 
     }
 
-    public void start(int workMins, int breakMins, int longBreakMins, int breakIntervals)
+    public void start(int workMins, int breakMins, int longBreakMins, int breakIntervals, boolean resume)
     {
-        this.workMins = workMins;
-        this.breakMins = breakMins;
-        this.breakItervals = breakIntervals;
-        this.longBreakMins = longBreakMins;
-        currSeconds = 15;
-        currMins = workMins;
+        if(!resume)
+        {
+            this.workMins = workMins;
+            this.breakMins = breakMins;
+            this.breakItervals = breakIntervals;
+            this.longBreakMins = longBreakMins;
+            currSeconds = 0;
+            currMins = workMins;
+        }
+
+        t = new Timer();
+
+
+        TimerTask task = new TimerTask() {
+            public void run() {
+                if(currSeconds == 0)
+                {
+                    currMins--;
+                    currSeconds = 60;
+                }
+
+                currSeconds--;
+
+                if(currSeconds < 10) {
+                    MainApplication.app.UpdateTimer(currMins + " : 0" + currSeconds);
+                }
+                else
+                {
+                    MainApplication.app.UpdateTimer(currMins + " : " + currSeconds);
+                }
+            }
+
+        };
         t.scheduleAtFixedRate(task,1000,1000);
     }
 }
